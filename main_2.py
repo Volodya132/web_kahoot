6b75232d0a2baca8fd952343263e416aa2b719e2
Volodya132 <56843184+Volodya132@users.noreply.github.com>
2023-12-07 19:32:23 +0200

    lesson2

--- main_2.py
+++ main_2.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-
 from SQLAgent import SQLAgent
 
 app = Flask(__name__)
@@ -11,15 +10,17 @@ DB_NAME = "quizz.db"
 def index():
     sql_agent = SQLAgent(DB_NAME)
     quizzes = sql_agent.get_quizzes()
-    print(quizzes)
+
     return render_template("index.html", quizzes=quizzes)
 
 @app.route("/quizzes/<int:quizz_id>")
 def quizz(quizz_id):
     sql_agent = SQLAgent(DB_NAME)
     questions = sql_agent.get_questions(quizz_id)
+
     session["questions"] = questions
     session["current_question"] = 0
+    session["count_of_correct_answers"] = 0
     return redirect(url_for("question", quizz_id=quizz_id))
 
 
@@ -29,6 +30,10 @@ def question(quizz_id):
     question_index = session["current_question"]
     questions = session.get('questions', [])
 
+    count_of_questions = len(questions)
+
+    if question_index >= count_of_questions:
+       return redirect(url_for("result", quizz_id=quizz_id))
     q= questions[question_index]
     options = db.get_options_for_question(q[0])
 
@@ -37,14 +42,14 @@ def question(quizz_id):
 
 @app.route("/quizzes/<int:quizz_id>/answer", methods=['POST'])
 def answer_func(quizz_id):
-    print(123)
+    print(request.form)
     session['current_question'] += 1
-
     return redirect(url_for('question', quizz_id=quizz_id))
 
 @app.route("/quizzes/<int:quizz_id>/result")
-def result(result):
-    pass
+def result(quizz_id):
+    return render_template('result.html')
+
 
 
 app.run(debug=True)
